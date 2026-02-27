387c02ea1d738232ecb14322817dc16a28912201
Chris Dixon <43393562+Dixon10@users.noreply.github.com>
2026-02-27 15:55:39 -0600

    Added Rules Required Fault Logic (#6)

--- Core/Inc/conditions_and_utils.h
+++ Core/Inc/conditions_and_utils.h
@@ -1,9 +1,13 @@
 #ifndef INC_CONDITIONS_AND_UTILS_H_
 #define INC_CONDITIONS_AND_UTILS_H_
 
+#include <stdbool.h>
 // LED Code
 #define HBEAT_LED_DELAY_TIME_ms 500
 void LED_task();
 
+// Sensor Faults/Limits
+float calculate_dc_current_limit();
+bool is_vehicle_faulting();
 
 #endif /* INC_CONDITIONS_AND_UTILS_H_ */
